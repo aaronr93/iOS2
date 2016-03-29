13c1b9ba648cd949f604f528da93f60904226d30
Zach Nafziger <zachary@nafziger.net>
2016-03-29 11:28:34 -0400

    really minor change

--- Pocket Grovers/Pocket Grovers/UserTweets.swift
+++ Pocket Grovers/Pocket Grovers/UserTweets.swift
@@ -4,14 +4,18 @@
 //
 //  Created by Aaron Rosenberger on 3/21/16.
 //  Copyright © 2016 Nafberger Games. All rights reserved.
-//
+//d
 
 import Foundation
 
 class UserTweets: NSObject {
     var tweets = [Tweet]()
-    
+    var username: String!
     func load() {
+        //GET https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=\(username)
         
+    }
+    init(username:String){
+        self.username = username
     }
 }
